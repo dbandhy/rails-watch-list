91253b437aa5c938a3214516957cc0643507a185
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <diegoby1994@gmail.com>
2024-08-29 15:56:47 -0500

    controllers done

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  get '/lists', to: 'movies#index', as: 'lists' # user see all movie list
+  get '/lists/:id', to: 'lists#show' # user can see list details
+  post '/lists', to: 'lists#create' # user can create a new list
+
   # Defines the root path route ("/")
   # root "posts#index"
 end
